65f5a5f0365b87ad150a4975d5ccd48f0572a897
bcoghi <bcoghill601@gmail.com>
2024-10-30 15:06:47 -0400

    Add files via upload

---
+++ test_parse.py
@@ -0,0 +1,17 @@
+import csv
+
+# Specify the filename
+filename = 'C:\\Users\\bcoghi\\Downloads\\netflow_day-02\\data.csv'
+
+# Open the CSV file
+with open(filename, mode='r', newline='') as csvfile:
+    # Create a CSV reader
+    csvreader = csv.reader(csvfile)
+    
+    # Get the header (first row)
+    header = next(csvreader)
+    print(f'Header: {header}')
+    
+    # Read the remaining rows
+    for row in csvreader:
+        print(row)
