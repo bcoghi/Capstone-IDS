33e1e55b0e497d333e012a6eda08e297fd29cf3f
bcoghi <bcoghill601@gmail.com>
2024-10-31 00:39:50 -0400

    Work on csv v2

--- csv_parse_2.py
+++ csv_parse_2.py
@@ -1,3 +1,13 @@
+"""
+TODO
+The actual net data exceeds 25 GB mem usage before even finishing currently
+Eventually hits memory error on my machine (32GB)
+Need to cut that down
+    -Simplify data structure
+    -???
+    -less data lmao
+"""
+
 import csv
 
 # Specify the filename
@@ -12,9 +22,12 @@ with open(filename, mode='r+', newline='') as csvfile:
     fieldnames = ["Time","Duration","SrcDevice","DstDevice","Protocol","SrcPort","DstPort","SrcPackets","DstPackets","SrcBytes","DstBytes"]
     csvreader = csv.DictReader(csvfile,fieldnames)
     
+    #row_count = sum(1 for row in csvreader) /2
+    #print(row_count)
     #iterate over each row
-    for i, row in csvreader:
+    for row in csvreader:
         #add each row (in dictionary format) to the end of the list
+        #if i<row_count:
         dictList.append(row)
         
     #print list for testing
