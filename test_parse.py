491c7c8735b366ce88266dd1bfb490d64e3d9d94
bcoghi <bcoghill601@gmail.com>
2024-11-04 09:09:16 -0500

    nov4

--- test_parse.py
+++ test_parse.py
@@ -1,17 +1,22 @@
 import csv
 
 # Specify the filename
-filename = 'C:\\Users\\bcoghi\\Downloads\\netflow_day-02\\data.csv'
-
+filename = 'data.csv'
+data_list = []
 # Open the CSV file
-with open(filename, mode='r', newline='') as csvfile:
+with open(filename, mode='r+', newline='') as csvfile:
     # Create a CSV reader
-    csvreader = csv.reader(csvfile)
-    
+    csvreader = csv.DictReader(csvfile)
+    csvwriter = csv.writer(csvfile)
+    #csvwriter.writerow(["Time","Duration","SrcDevice","DstDevice","DstPackets","SrcBytes","DstBytes"])
+    csvwriter.writerow(["Time","Duration","SrcDevice","DstDevice","DstPackets","SrcBytes","DstBytes"])
     # Get the header (first row)
-    header = next(csvreader)
-    print(f'Header: {header}')
+    #header = next(csvreader)
+    #print(f'Header: {header}')
     
     # Read the remaining rows
     for row in csvreader:
         print(row)
+        data_list = [row for row in csvreader]
+
+    print(data_list)
